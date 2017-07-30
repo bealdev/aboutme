4d083d9ecc772801f71ab63d894aa47eddc35968
bealdev <30557622+bealdev@users.noreply.github.com>
2017-07-30 16:35:15 -0400

    troubleshooting
    email validation

--- js/email.js
+++ js/email.js
@@ -1,12 +1,19 @@
 $('form input[name="email"]').blur(function () {
     var email = $(this).val();
 var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm;
-if (re.test(email)) {
-    $('.msg').hide();
-    $('.success').show();
-} else {
-    $('.msg').hide();
-    $('.error').show();
+if(email != NULL) {
+	if (re.test(email)) {
+	    $('.msg').show();
+	    $('.error').show();
+	} 
 }
+if (email == NULL) {
+    $('.msg').hide();
+    $('.error').hide();
+}
+if (!re.test(email)) {
+    $('.msg').hide();
+    $('.error').hide();
+} 
 
 });
