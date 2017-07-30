7aec8a521d8e9bf47d61b697ec892372783f5601
bealdev <30557622+bealdev@users.noreply.github.com>
2017-07-30 16:22:02 -0400

    update
    email validation added

---
+++ js/email.js
@@ -0,0 +1,12 @@
+$('form input[name="email"]').blur(function () {
+    var email = $(this).val();
+var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm;
+if (re.test(email)) {
+    $('.msg').hide();
+    $('.success').show();
+} else {
+    $('.msg').hide();
+    $('.error').show();
+}
+
+});
