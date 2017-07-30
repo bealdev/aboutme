bfaedee884252f171d6673c402d8ecb4b75815fb
bealdev <30557622+bealdev@users.noreply.github.com>
2017-07-30 16:55:45 -0400

    troubleshooting

--- js/email.js
+++ js/email.js
@@ -2,11 +2,11 @@ $('form input[name="email"]').blur(function () {
     var email = $(this).val();
 var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm;
 if (re.test(email)) {
-    $('.msg').show();
-    $('.error').show();
-} 
-if (!re.test(email)) {
     $('.msg').hide();
     $('.error').hide();
+} 
+if (!re.test(email)) {
+    $('.msg').show();
+    $('.error').show();
 } 
 });
