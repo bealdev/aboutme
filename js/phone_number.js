0418eb59f9c2b74d829953d695054b856e71733d
bealdev <30557622+bealdev@users.noreply.github.com>
2017-07-30 20:47:28 -0400

    update
    general update

--- js/phone_number.js
+++ js/phone_number.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
      //if the letter is not digit then display error and don't type anything
      if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
         //display error message
-        $("#errmsg").html("You sure I can call this?").show().fadeOut(7000);
+        $("#errmsg").html("You sure I can call this?").delay(5000).show(0);
                return false;
     }
    });
